d559c1e2e0b8704c4109cec9632f038bae2a2190
Sidney Alcantara <sidney@sidney.me>
2020-05-05 00:11:59 +1000

    Theme: set TextField variant prop to filled by default

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -296,6 +296,7 @@ export const defaultOverrides: ThemeOptions = {
       underline: 'hover',
     },
     MuiChip: { deleteIcon: <ClearIcon /> },
+    MuiTextField: { variant: 'filled' },
   },
 };
 
