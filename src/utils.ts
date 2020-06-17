249c8e0f8d9794210fa97a5d42c8c070f7d49539
Sidney Alcantara <sidney@sidney.me>
2020-06-17 16:33:46 +1000

    fix utils export

--- src/utils.ts
+++ src/utils.ts
@@ -4,3 +4,5 @@ export const lineBreakToBr = (str?: string) =>
   typeof str === 'string'
     ? DOMPurify.sanitize(str.replace(/\n\s*\n/g, '<div class="spacer"></div>'))
     : '';
+
+export default 'ANTLER UTILS';
